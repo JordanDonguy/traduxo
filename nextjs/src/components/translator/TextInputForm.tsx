c6356958b022c2ac03211f8c16386b82eecc297f
JordanDonguy <jordan.donguy@gmail.com>
2025-11-24 11:32:31 +0100

    style(components): update TextInputForm and LanguageSelector, adjust MainDisplay

--- nextjs/src/components/translator/TextInputForm.tsx
+++ nextjs/src/components/translator/TextInputForm.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { Mic, CircleStop } from "lucide-react";
+import { Mic, CircleStop, SendHorizontal, Dices } from "lucide-react";
+import { FormEvent, useRef, useState } from "react";
+import { useSuggestion } from "@traduxo/packages/hooks/suggestion/useSuggestion";
 
 type TextInputFormProps = {
   inputText: string;
@@ -17,50 +19,102 @@ export default function TextInputForm({
   isListening,
   handleVoice
 }: TextInputFormProps) {
+  const [animateSend, setAnimateSend] = useState(false);
+  const typingTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  const { suggestTranslation, isRolling } = useSuggestion({});
+
+  const handleSubmit = ((e: FormEvent<Element>) => {
+    e.preventDefault()
+    if (typingTimeout.current) {
+      clearTimeout(typingTimeout.current);
+      typingTimeout.current = null; // mark it cleared
+    }
+    setAnimateSend(false); // make sure animation doesn't trigger
+    handleTranslate(e);
+  });
+
   return (
-    <section className="w-full h-1/2 flex items-center justify-center">
-      <form
-        id="text-input-form"
-        aria-label="Text input form"
-        data-testid="input-form"
-        onSubmit={handleTranslate}
-        className="w-[90%] flex bg-[var(--input)] rounded-r-4xl rounded-l-2xl h-12 border border-[var(--input-border)] mt-2"
-      >
-        <input
-          id="text-input"
-          aria-label="Enter text to translate"
-          className="w-full h-full px-6 focus:outline-none"
-          placeholder="Enter some text..."
-          onChange={(e) => setInputText(e.target.value)}
-          value={inputText}
-          autoComplete="off"
-          maxLength={100}
-        />
+    <form
+      id="text-input-form"
+      aria-label="Text input form"
+      data-testid="input-form"
+      onSubmit={handleSubmit}
+      className="col-span-1 flex flex-col justify-between pt-4 pb-2 px-2 md:px-6 border border-zinc-500 rounded-md font-semibold shadow-sm"
+    >
+      <textarea
+        id="text-input"
+        aria-label="Enter text to translate"
+        className="w-full flex-1 text-2xl focus:outline-none resize-none pl-2"
+        placeholder="Enter some text..."
+        onChange={(e) => {
+          setInputText(e.target.value);
+
+          // reset waiting + animation
+          setAnimateSend(false);
+
+          // clear old timer
+          if (typingTimeout.current) clearTimeout(typingTimeout.current);
+
+          if (e.target.value.length > 0) {
+            // start new timer if there's some text in input
+            typingTimeout.current = setTimeout(() => {
+              setAnimateSend(true); // triggers bounce
+            }, 2000);
+          }
+        }}
+        value={inputText}
+        autoComplete="off"
+        maxLength={100}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && !e.shiftKey) {
+            handleSubmit(e);
+          }
+        }}
+      />
+
+      <div className="flex justify-between items-center">
+
+        <div className="flex">
+          <button
+            id="voice-input-button"
+            aria-label="Voice input"
+            data-testid="mic-button"
+            type="button"
+            className={`w-10 h-10 hover:cursor-pointer hover:bg-[var(--hover)] rounded-full 
+            ${isListening && "text-red-400"} flex justify-center items-center`}
+            onClick={handleVoice}
+          >
+            {!isListening ? <Mic /> : <CircleStop />}
+          </button>
+
+          <button
+            id="suggestion-button-mobile"
+            aria-label="Suggest an expression"
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              suggestTranslation();
+            }}
+            className="hidden md:block p-2 rounded-full hover:bg-[var(--hover)] hover:cursor-pointer"
+          >
+            <Dices size={28} className={`${isRolling ? "animate-dice-roll" : ""}`} />
+          </button>
+
+        </div>
+
+        <div className="text-neutral-500">{inputText.length} / 100</div>
 
         <button
           id="submit-translation-button"
           aria-label="Translate"
           type="submit"
-          className="sr-only"
+          className={`w-10 h-10 hover:cursor-pointer hover:bg-[var(--hover)] rounded-full flex justify-center items-center
+            ${animateSend ? "ping-once" : ""}`}
         >
-          Translate
+          <SendHorizontal />
         </button>
-
-        <button
-          id="voice-input-button"
-          aria-label="Voice input"
-          data-testid="mic-button"
-          type="button"
-          className="w-12 hover:cursor-pointer hover:bg-[var(--hover)] rounded-full flex justify-center items-center"
-          onClick={handleVoice}
-        >
-          {!isListening ? <Mic /> : <CircleStop />}
-        </button>
-      </form>
-
-      {inputText.length === 100 && (
-        <p className="text-sm absolute bottom-1 left-[5%] text-neutral-400 italic">100 characters max allowed</p>
-      )}
-    </section>
+      </div>
+    </form>
   );
 }
