84f6580f4b8ed7e05439978036fb0a39214392d1
JordanDonguy <jordan.donguy@gmail.com>
2025-11-24 12:00:15 +0100

    refactor(hooks): make useLanguageSwitch delay injectable for RN and web

--- react_native/components/translator/TranslatorInput.tsx
+++ react_native/components/translator/TranslatorInput.tsx
@@ -39,6 +39,7 @@ export default function TranslatorInput({ inputRef }: { inputRef: React.RefObjec
     setInputLang,
     setOutputLang,
     detectedLang,
+    delay: 80
   });
 
   const insets = useSafeAreaInsets();
