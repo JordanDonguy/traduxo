11838becb4568e90df71d635a174d9d03eb5bc81
JordanDonguy <jordan.donguy@gmail.com>
2025-11-25 23:35:43 +0100

    feat(ui): add a loading page

---
+++ nextjs/src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function LoadingPage() {
+  return (
+    <div className="bg-[var(--menu)] min-h-screen pb-12 bg-opacity-60 z-40 flex items-center justify-center">
+      <div className="spinner" />
+    </div>
+  )
+}
